a3699bb160cb173e44f7da4e0f19544af60e8835
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-07-22 15:42:53 +0100

    Add BlocObserver implementation for enhanced state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:counter_app/bloc/counter_bloc.dart';
+import 'package:counter_app/bloc_observer.dart';
 import 'package:counter_app/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
+  Bloc.observer = MyObserver();
   runApp(CounterApp());
 }
 
