fd5eaa6225513ae7f0fded75d1b6a0b7503a36c2
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-07-22 15:48:00 +0100

    Fix formatting in CounterBloc: add spaces for consistency in emit statements

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -7,10 +7,10 @@ part 'counter_state.dart';
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super((InitialState(0))) {
     on<IncrementEvent>((event, emit) {
-      emit(IncrementState(state.counter +1));
+      emit(IncrementState(state.counter + 1));
     });
     on<DecrementEvent>((event, emit) {
-      emit(DecrementState(state.counter -1));
+      emit(DecrementState(state.counter - 1));
     });
     on<ResetEvent>((event, emit) {
       emit(RsetState(0));
