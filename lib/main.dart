e247d4438481487896934fd44f99e5ef23639a26
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-07-22 10:33:51 +0100

    Implement main app structure with CounterBloc and HomePage UI

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:counter_app/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CounterApp());
+}
+
+class CounterApp extends StatelessWidget {
+  const CounterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
