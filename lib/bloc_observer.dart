21fba3d8a3c36b0b24675d39a36f54845c6cacc1
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-07-23 16:22:54 +0100

    Remove commented print statements from BlocObserver for cleaner code

--- lib/bloc_observer.dart
+++ lib/bloc_observer.dart
@@ -4,28 +4,28 @@ class MyObserver extends BlocObserver {
   @override
   void onCreate(BlocBase bloc) {
     super.onCreate(bloc);
-    print(bloc);
+    //print(bloc);
   }
 
   @override
   void onEvent(Bloc bloc, Object? event) {
     super.onEvent(bloc, event);
-    print(bloc);
-    print(event);
+    //print(bloc);
+    //print(event);
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
-    print(bloc);
-    print(change);
+    //print(bloc);
+    //print(change);
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
     super.onTransition(bloc, transition);
-    print(bloc);
-    print(transition);
+    //print(bloc);
+    //print(transition);
   }
 
   @override
@@ -36,8 +36,8 @@ class MyObserver extends BlocObserver {
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
     super.onError(bloc, error, stackTrace);
-    print(bloc);
-    print(error);
-    print(stackTrace);
+    //print(bloc);
+    //print(error);
+    //print(stackTrace);
   }
 }
