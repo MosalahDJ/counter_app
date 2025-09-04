a0039bcbed3d7e5f2e6fb06cd51ae4ac3ee3153e
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-09-04 14:29:33 +0100

    change parsts of pr

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:counter_app/bloc/counter_bloc.dart';
-import 'package:counter_app/bloc_observer.dart';
+import 'package:counter_app/EX/bloc/counter_bloc.dart';
+import 'package:counter_app/EX/bloc_observer.dart';
 import 'package:counter_app/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
