35f6ca9bfcc38a987a3f41ac272b9074a9840af9
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-09-04 14:28:33 +0100

    create new cubit

--- lib/EXhome_page.dart
+++ lib/EXhome_page.dart
@@ -2,8 +2,8 @@ import 'package:counter_app/bloc/counter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class EXHomePage extends StatelessWidget {
+  const EXHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
